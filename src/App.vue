66c239a825725571b97ce0e4abba2ec4808647b3
nkosi-tauro <nkosilati23@yahoo.com>
2021-02-12 18:16:03 +0200

    todo init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Todo/>
+  </div>
+</template>
+
+<script>
+import Todo from './components/Todo.vue'
+export default {
+  name: 'App',
+  components: {
+    Todo
+  }
+
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <h1>Pizza</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Todo'
+
+}
+</script>
+
+<style>
+
+</style>
