aa855c814af341d173d56e3e72327921bcb11aa7
nkosi-tauro <nkosilati23@yahoo.com>
2021-02-12 18:46:02 +0200

    todoApp

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,16 +1,69 @@
 <template>
   <div>
-      <h1>Pizza</h1>
+    <h1>ToDo List</h1>
+    <ul>
+      <li v-for="(todo, index) in todosList" :key="index">
+        <div>
+            <p>{{ todo.title }}</p>
+            <p>{{ todo.desc }}</p>
+            <button @click="removeTodo(index)">❌</button>
+        </div>
+      </li>
+    </ul>
+
+    <form @submit.prevent="addTodo">
+        <input v-model.trim="newTodo.title" required/>
+        <br>
+        <input v-model.trim="newTodo.desc" required/>
+        <button type="submit">Add Todo</button>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'Todo'
+    name: 'Todo',
+    methods: {
+        addTodo(){
+            if(this.newTodo.title !== '' && this.newTodo.desc !== ''){
+                this.todosList.push({title : this.newTodo.title, desc : this.newTodo.desc})
+                this.newTodo.title = ''
+                this.newTodo.desc = ''
+            }
+        },
 
+        removeTodo(index){
+            this.todosList = this.todosList.filter((todo, i)=> i != index)
+        }
+    },
+
+    data(){
+        return{
+            newTodo: {
+                title : "",
+                desc : ""
+            },
+            todosList : [
+                {   
+                    title : "Pizza",
+                    desc : "Hawaiian"
+                
+                },
+                {   
+                    title : "Pizza",
+                    desc : "Mexican"
+                
+                },
+                {   
+                    title : "Pizza",
+                    desc : "Macaroni"
+                
+                }
+            ]
+        }
+    }
 }
 </script>
 
 <style>
-
 </style>
