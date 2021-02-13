4c4fd9e308d087abcad412d37b7e79d4dc36c229
nkosi-tauro <nkosilati23@yahoo.com>
2021-02-13 11:26:15 +0200

    added Login component

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div>
     <Header/>
     <Todo/>
+    <LoginModal/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
 import Todo from './components/Todo.vue'
+import LoginModal from './components/LoginModal.vue'
 
 export default {
   name: 'App',
   components: {
     Todo,
-    Header
+    Header,
+    LoginModal
   }
 
 }
